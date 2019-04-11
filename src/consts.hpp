5ece61b485191347ec4f2c3f9d52f966e251621b
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-04-11 12:01:34 -0400

    Add constants

--- src/consts.hpp
+++ src/consts.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
 #include "types.hpp"
+#include "vector.hpp"
 
 namespace engine
 {
 	const real PI = 3.14159265359;
+
+	const vector XAxis{ 1, 0, 0 };
+	const vector YAxis{ 0, 1, 0 };
+	const vector ZAxis{ 0, 0, 1 };
+	const point Origin{ 0, 0, 0 };
 }
