84fb588c17c54b8434bf957ce8361a47ae1fd126
Martin Vorbrodt <martin.vorbrodt@icloud.com>
2019-04-22 13:11:54 -0400

    Compute local to global transform matrix by walking the nodes

--- src/asset_info.cpp
+++ src/asset_info.cpp
@@ -55,23 +55,26 @@ int main(int argc, char** argv)
 	const aiNode* root = scene->mRootNode;
 
 	cout << endl;
-	cout << "Animations: " << scene->mNumAnimations << endl;
-	cout << "Cameras   : " << scene->mNumCameras << endl;
-	cout << "Lights    : " << scene->mNumLights << endl;
-	cout << "Materials : " << scene->mNumMaterials << endl;
-	cout << "Textures  : " << scene->mNumTextures << endl;
-	cout << "Meshes    : " << scene->mNumMeshes << endl;
+	cout << "Root node      : " << root->mName.C_Str() << endl;
+	cout << "Root transform : " << (root->mTransformation.IsIdentity() ? "no" : "yes") << endl << endl;
+
+	cout << "Animations : " << scene->mNumAnimations << endl;
+	cout << "Cameras    : " << scene->mNumCameras << endl;
+	cout << "Lights     : " << scene->mNumLights << endl;
+	cout << "Materials  : " << scene->mNumMaterials << endl;
+	cout << "Textures   : " << scene->mNumTextures << endl;
+	cout << "Meshes     : " << scene->mNumMeshes << endl;
 
 	if (scene->HasCameras()) cout << endl;
 	for (unsigned int i = 0; i < scene->mNumCameras; ++i)
 	{
 		cout << "Camera:" << endl;
 		const aiCamera* camera = scene->mCameras[i];
-		cout << "\tName    : " << camera->mName.C_Str() << endl;
-		cout << "\tAspect  : " << camera->mAspect << endl;
-		cout << "\tFOV     : " << camera->mHorizontalFOV * 180.0f / 3.14159265359f * 2.0f << endl;
-		cout << "\tNear    : " << camera->mClipPlaneNear << endl;
-		cout << "\tFar     : " << camera->mClipPlaneFar << endl;
+		cout << "\tName     : " << camera->mName.C_Str() << endl;
+		cout << "\tAspect   : " << camera->mAspect << endl;
+		cout << "\tFOV      : " << camera->mHorizontalFOV * 180.0f / 3.14159265359f * 2.0f << endl;
+		cout << "\tNear     : " << camera->mClipPlaneNear << endl;
+		cout << "\tFar      : " << camera->mClipPlaneFar << endl;
 
 		const aiNode* node = root->FindNode(camera->mName);
 		assert(node->mParent == root);
@@ -80,9 +83,9 @@ int main(int argc, char** argv)
 		aiVector3D look_at = (aiMatrix3x3(transformation) * camera->mLookAt).Normalize();
 		aiVector3D up = (aiMatrix3x3(transformation) * camera->mUp).Normalize();
 
-		cout << "\tPosition: (X: " << position.x << ", Y: " << position.y << ", Z: " << position.z << ")" << endl;
-		cout << "\tLook At : (X: " << look_at.x << ", Y: " << look_at.y << ", Z: " << look_at.z << ")" << endl;
-		cout << "\tUp      : (X: " << up.x << ", Y: " << up.y << ", Z: " << up.z << ")" << endl;
+		cout << "\tPosition : (X: " << position.x << ", Y: " << position.y << ", Z: " << position.z << ")" << endl;
+		cout << "\tLook At  : (X: " << look_at.x << ", Y: " << look_at.y << ", Z: " << look_at.z << ")" << endl;
+		cout << "\tUp       : (X: " << up.x << ", Y: " << up.y << ", Z: " << up.z << ")" << endl;
 		cout << endl;
 	}
 
@@ -91,8 +94,8 @@ int main(int argc, char** argv)
 	{
 		cout << "Light:" << endl;
 		const aiLight* light = scene->mLights[i];
-		cout << "\tName      : " << light->mName.C_Str() << endl;
-		cout << "\tType      : ";
+		cout << "\tName       : " << light->mName.C_Str() << endl;
+		cout << "\tType       : ";
 		switch (light->mType)
 		{
 			case aiLightSource_UNDEFINED:   cout << "Undefined" << endl; break;
@@ -107,15 +110,15 @@ int main(int argc, char** argv)
 		aiVector3D position = transformation * light->mPosition;
 		aiVector3D direction = (aiMatrix3x3(transformation) * light->mDirection).Normalize();
 
-		cout << "\tPosition  : (X: " << position.x << ", Y: " << position.y << ", Z: " << position.z << ")" << endl;
-		cout << "\tDirection : (X: " << direction.x << ", Y: " << direction.y << ", Z: " << direction.z << ")" << endl;
-		cout << "\tAmbient   : (R: " << light->mColorAmbient.r << ", G: " << light->mColorAmbient.g << ", B: " << light->mColorAmbient.b << ")" << endl;
-		cout << "\tDiffuse   : (R: " << light->mColorDiffuse.r << ", G: " << light->mColorDiffuse.g << ", B: " << light->mColorDiffuse.b << ")" << endl;
-		cout << "\tSpecular  : (R: " << light->mColorSpecular.r << ", G: " << light->mColorSpecular.g << ", B: " << light->mColorSpecular.b << ")" << endl;
-		cout << "\tCone angle: (Inner: " << light->mAngleInnerCone << ", Outer: " << light->mAngleOuterCone << ")" << endl;
-		cout << "\tAttenuation constant : " << light->mAttenuationConstant << endl;
-		cout << "\tAttenuation linear   : " << light->mAttenuationLinear << endl;
-		cout << "\tAttenuation quadratic: " << light->mAttenuationQuadratic << endl;
+		cout << "\tPosition   : (X: " << position.x << ", Y: " << position.y << ", Z: " << position.z << ")" << endl;
+		cout << "\tDirection  : (X: " << direction.x << ", Y: " << direction.y << ", Z: " << direction.z << ")" << endl;
+		cout << "\tAmbient    : (R: " << light->mColorAmbient.r << ", G: " << light->mColorAmbient.g << ", B: " << light->mColorAmbient.b << ")" << endl;
+		cout << "\tDiffuse    : (R: " << light->mColorDiffuse.r << ", G: " << light->mColorDiffuse.g << ", B: " << light->mColorDiffuse.b << ")" << endl;
+		cout << "\tSpecular   : (R: " << light->mColorSpecular.r << ", G: " << light->mColorSpecular.g << ", B: " << light->mColorSpecular.b << ")" << endl;
+		cout << "\tCone angle : (Inner: " << light->mAngleInnerCone << ", Outer: " << light->mAngleOuterCone << ")" << endl;
+		cout << "\tAttenuation constant  : " << light->mAttenuationConstant << endl;
+		cout << "\tAttenuation linear    : " << light->mAttenuationLinear << endl;
+		cout << "\tAttenuation quadratic : " << light->mAttenuationQuadratic << endl;
 		cout << endl;
 	}
 
@@ -147,31 +150,39 @@ int main(int argc, char** argv)
 	{
 		cout << "Mesh:" << endl;
 		const aiMesh* mesh = scene->mMeshes[i];
-		cout << "\tName              : " << mesh->mName.C_Str() << endl;
+		cout << "\tName                : " << mesh->mName.C_Str() << endl;
 
 		const aiNode* node = root->FindNode(mesh->mName);
-		assert(node->mParent == root);
-		aiMatrix4x4 transformation = node->mTransformation;
-		cout << "\tHas transformation: " << ((transformation != aiMatrix4x4()) ? "yes" : "no") << endl;
+		aiMatrix4x4 transformation;
+		if(node != nullptr)
+		{
+			while(node != nullptr)
+			{
+				cout << "\tNode name           : " << node->mName.C_Str() << endl;
+				cout << "\tNode transformation : " << (node->mTransformation.IsIdentity() ? "no" : "yes") << endl;
+				transformation = node->mTransformation * transformation;
+				node = node->mParent;
+			}
+		}
 
-		cout << "\tHas positions     : " << (mesh->HasPositions() ? "yes" : "no") << endl;
-		cout << "\tHas faces         : " << (mesh->HasFaces() ? "yes" : "no") << endl;
-		cout << "\tHas vertex colors : " << (mesh->HasVertexColors(0) ? "yes" : "no") << endl;
-		cout << "\tHas texture coords: " << (mesh->HasTextureCoords(0) ? "yes" : "no") << endl;
-		cout << "\tHas normals       : " << (mesh->HasNormals() ? "yes" : "no") << endl;
-		cout << "\tHas tan/bitan     : " << (mesh->HasTangentsAndBitangents() ? "yes" : "no") << endl;
-		cout << "\tHas bones         : " << (mesh->HasBones() ? "yes" : "no") << endl;
-		cout << "\tColor channels    : " << mesh->GetNumColorChannels() << endl;
-		cout << "\tUV channels       : " << mesh->GetNumUVChannels() << endl;
-		cout << "\tUV components     : " << mesh->mNumUVComponents[0] << endl;
-		cout << "\tMaterial index    : " << mesh->mMaterialIndex << endl;
-		cout << "\tNumber of vertices: " << mesh->mNumVertices << endl;
-		cout << "\tNumber of faces   : " << mesh->mNumFaces << endl;
-		cout << "\tNumber of bones   : " << mesh->mNumBones << endl;
-		if (mesh->mPrimitiveTypes & aiPrimitiveType_POINT)    cout << "\tPrimitive type    : Point" << endl;
-		if (mesh->mPrimitiveTypes & aiPrimitiveType_LINE)     cout << "\tPrimitive type    : Line" << endl;
-		if (mesh->mPrimitiveTypes & aiPrimitiveType_TRIANGLE) cout << "\tPrimitive type    : Triangle" << endl;
-		if (mesh->mPrimitiveTypes & aiPrimitiveType_POLYGON)  cout << "\tPrimitive type    : Polygon" << endl;
+		cout << "\tHas positions       : " << (mesh->HasPositions() ? "yes" : "no") << endl;
+		cout << "\tHas faces           : " << (mesh->HasFaces() ? "yes" : "no") << endl;
+		cout << "\tHas vertex colors   : " << (mesh->HasVertexColors(0) ? "yes" : "no") << endl;
+		cout << "\tHas texture coords  : " << (mesh->HasTextureCoords(0) ? "yes" : "no") << endl;
+		cout << "\tHas normals         : " << (mesh->HasNormals() ? "yes" : "no") << endl;
+		cout << "\tHas tan/bitan       : " << (mesh->HasTangentsAndBitangents() ? "yes" : "no") << endl;
+		cout << "\tHas bones           : " << (mesh->HasBones() ? "yes" : "no") << endl;
+		cout << "\tColor channels      : " << mesh->GetNumColorChannels() << endl;
+		cout << "\tUV channels         : " << mesh->GetNumUVChannels() << endl;
+		cout << "\tUV components       : " << mesh->mNumUVComponents[0] << endl;
+		cout << "\tMaterial index      : " << mesh->mMaterialIndex << endl;
+		cout << "\tNumber of vertices  : " << mesh->mNumVertices << endl;
+		cout << "\tNumber of faces     : " << mesh->mNumFaces << endl;
+		cout << "\tNumber of bones     : " << mesh->mNumBones << endl;
+		if (mesh->mPrimitiveTypes & aiPrimitiveType_POINT)    cout << "\tPrimitive type      : Point" << endl;
+		if (mesh->mPrimitiveTypes & aiPrimitiveType_LINE)     cout << "\tPrimitive type      : Line" << endl;
+		if (mesh->mPrimitiveTypes & aiPrimitiveType_TRIANGLE) cout << "\tPrimitive type      : Triangle" << endl;
+		if (mesh->mPrimitiveTypes & aiPrimitiveType_POLYGON)  cout << "\tPrimitive type      : Polygon" << endl;
 		cout << endl;
 	}
 }
